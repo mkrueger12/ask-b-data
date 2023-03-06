a6ef70c9d3fd8f17f36af0b9a261de60595cfe3f
Max K <max@kruegers.om>
2023-03-05 17:57:14 -0700

    fix: fixed error with empty data

--- avalanche/avalanche/data/collection/streamflows.py
+++ avalanche/avalanche/data/collection/streamflows.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 
+
 def get_streamflow_station_ids(state):
     # The URL for the USGS streamflow stations service in the specified state
     url = f'https://waterservices.usgs.gov/nwis/site/?format=rdb&stateCd={state}&siteType=ST&hasDataTypeCd=iv'
@@ -22,10 +23,6 @@ def get_streamflow_data(url, station_id):
     response = response.json()
     print(url)
 
-
-    # Extract the time series data from the json object
-    time_series = response['value']['timeSeries'][3]['values'][0]['value']
-
     # Extract the time series data from the json object
     time_series = None
     for ts in response['value']['timeSeries']:
@@ -35,11 +32,12 @@ def get_streamflow_data(url, station_id):
 
     # If time_series is still None, no data was found for variableCode '00060'
     if time_series is None:
-        raise ValueError("No data found for variableCode '00060'")
+        print(f"No data found for variableCode '00060' for station_id: {station_id}")
+        return pd.DataFrame()
 
     # Create a list to store the data
-    data = [{'datetime': ts['dateTime'], 'discharge': float(ts['value']), 'station_id': station_id} for ts in time_series]
-
+    data = [{'datetime': ts['dateTime'], 'discharge': float(ts['value']), 'station_id': station_id} for ts in
+            time_series]
 
     # Create a pandas dataframe from the data list
     df = pd.DataFrame(data)
@@ -52,10 +50,11 @@ ids = get_streamflow_station_ids('CO')
 df = []
 
 for i in ids:
+    data = get_streamflow_data(
+        f'https://waterservices.usgs.gov/nwis/dv/?format=json&sites={i}&period=P365D&siteStatus=active', i)
+    if not data.empty:
+        df.append(data)
 
-    data = get_streamflow_data(f'https://waterservices.usgs.gov/nwis/dv/?format=json&sites={i}&period=P365D&siteStatus=active', i)
-    df.append(data)
+# dfs = [get_streamflow_data(f'https://waterservices.usgs.gov/nwis/dv/?format=json&sites={i}&period=P365D&siteStatus=active') for i in ids]
 
-#dfs = [get_streamflow_data(f'https://waterservices.usgs.gov/nwis/dv/?format=json&sites={i}&period=P365D&siteStatus=active') for i in ids]
-
-#df = pd.concat(dfs)
+# df = pd.concat(dfs)
