d583986dc56433951cc27923553de32420be553c
Max K <max@kruegers.om>
2023-03-05 18:09:41 -0700

    feat: add comments

--- avalanche/avalanche/data/collection/streamflows.py
+++ avalanche/avalanche/data/collection/streamflows.py
@@ -1,8 +1,31 @@
-def get_streamflow_data(url, station_id):
-    # Send a GET request to the URL and store the response as a json object
-    print(f"Getting streamflow data for station {station_id} from URL: {url}")
+import pandas as pd
+import requests
+
+
+def get_streamflow_station_ids(state):
+    # The URL for the USGS streamflow stations service in the specified state
+    url = f'https://waterservices.usgs.gov/nwis/site/?format=rdb&stateCd={state}&siteType=ST&hasDataTypeCd=iv'
+
+    # Send a request to the USGS API
     response = requests.get(url)
-    print(f"Response status code: {response.status_code}")
+
+    # Parse the response into a list of station IDs
+    station_ids = [line.split('\t')[1] for line in response.text.split('\n') if line.startswith('USGS')]
+
+    # Return the list of station IDs
+    return station_ids
+
+
+def get_streamflow_data(url, station_id):
+    print(f"Fetching data for station {station_id} from {url}...")
+
+    # Send a GET request to the URL and store the response as a json object
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Error {response.status_code} while fetching data for station {station_id} from {url}")
+        return pd.DataFrame()
+
     response = response.json()
 
     # Extract the time series data from the json object
@@ -14,13 +37,29 @@ def get_streamflow_data(url, station_id):
 
     # If time_series is still None, no data was found for variableCode '00060'
     if time_series is None:
-        print(f"No data found for variableCode '00060' for station {station_id}, skipping.")
-        return None
+        print(f"No data found for variableCode '00060' for station_id: {station_id}")
+        return pd.DataFrame()
 
     # Create a list to store the data
-    data = [{'datetime': ts['dateTime'], 'discharge': float(ts['value']), 'station_id': station_id} for ts in time_series]
+    data = [{'datetime': ts['dateTime'], 'discharge': float(ts['value']), 'station_id': station_id} for ts in
+            time_series]
 
     # Create a pandas dataframe from the data list
     df = pd.DataFrame(data)
 
+    print(f"Fetched {len(df)} records for station {station_id}")
+
     return df
+
+
+ids = get_streamflow_station_ids('CO')
+
+dfs = []
+
+for i in ids:
+    data = get_streamflow_data(
+        f'https://waterservices.usgs.gov/nwis/dv/?format=json&sites={i}&period=P365D&siteStatus=active', i)
+    if not data.empty:
+        dfs.append(data)
+
+df = pd.concat(dfs)
